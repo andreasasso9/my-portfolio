6b30a7c2744d10befe19488db9681e19f493ac72
andreasasso9 <andry-sasso@outlook.it>
2025-07-06 18:42:06 +0200

    migliorato il component SendMail

--- components/SendMail/component.jsx
+++ components/SendMail/component.jsx
@@ -27,7 +27,7 @@ export default function SendMail() {
 	}
 
 	return (
-		<div className="flex flex-col justify-center gap-1">
+		<div className="flex flex-col justify-center gap-1 w-auto sm:w-lg">
 			<h1 className="text-base sm:text-lg">Send me an Email</h1>
 			<input id='email' type='email' placeholder='Insert your email' className="border rounded px-2 py-2 text-xs sm:text-base"></input>
 			<textarea id="message" rows={5} placeholder='Insert your message' className="border rounded px-2 py-2 text-xs sm:text-base"></textarea>
