4fdade66b542cd2eb144bdd03b1b7274d7e1dfd5
andreasasso9 <127236405+andreasasso9@users.noreply.github.com>
2025-09-15 16:06:45 +0200

    aggiunto underline su navbar

--- components/Navbar/component.jsx
+++ components/Navbar/component.jsx
@@ -5,10 +5,10 @@ import Link from 'next/link';
 export default function Navbar() {
 	return (
 		<div className="flex justify-between items-center font-bold gap-5 sm:gap-10 md:gap-15 max-w-full py-10 px-3 z-10 text-lg sm:text-2xl md:text-4xl">
-			<Link href="/">Home</Link>
-			<Link href="/About">About Me</Link>
-			<Link href="/Projects">Projects</Link>
-			<Link href="/Contact">Contact</Link>
+			<Link href="/" className='hover:underline'>Home</Link>
+			<Link href="/About" className='hover:underline'>About Me</Link>
+			<Link href="/Projects" className='hover:underline'>Projects</Link>
+			<Link href="/Contact" className='hover:underline'>Contact</Link>
 		</div>
 
 	);
